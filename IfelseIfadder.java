c7b001375f209fd6af0002b4579717c4d5ac1311
subbaiahbachira2-ai <subbaiahbachira2@gmail.com>
2026-03-26 12:30:58 +0530

    updated control structure

---
+++ IfelseIfadder.java
@@ -0,0 +1,15 @@
+public class ifelseifadder {
+public class Ifelseifadder {
+    public static void main(String[] args) {
+        int marks=83;
+        if(marks>=90){
+            System.out.println("A grade");
+        }else if(marks>80){
+            System.out.println("B grade");
+        }else if(marks>70){
+            System.out.println("c grade");
+        }else if(marks>60){
+            System.out.println("D grade");
+        }else
+            System.out.println("Fail");    
+}
