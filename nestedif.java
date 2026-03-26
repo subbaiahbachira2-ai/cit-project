c2a6ee46d511f1b2806b02f246dec479d3047ca1
subbaiahbachira2-ai <subbaiahbachira2@gmail.com>
2026-03-26 12:01:36 +0530

    updated control structure

---
+++ nestedif.java
@@ -0,0 +1,15 @@
+public class nestedif {
+    public static void main(String[] args) {
+        int age=65;
+        if(age>=18){
+            if(age>=65){
+                System.out.println("eligible to avail government fund");
+            }
+            else
+                System.out.println("we are not eligible to vote and avail government scheme");
+            }else
+                System.out.println("we are not eligibleto vote");
+
+        }
+    }
+
