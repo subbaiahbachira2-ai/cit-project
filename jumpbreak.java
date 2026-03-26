d5e8f3fc39a540c84d08462d6f14cc2861510cb7
subbaiahbachira2-ai <subbaiahbachira2@gmail.com>
2026-03-26 13:11:00 +0530

    updated control structure

---
+++ jumpbreak.java
@@ -0,0 +1,10 @@
+public class jumpbreak {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i==0)
+                //break;
+            continue;
+            System.out.println("value is:"+i);
+        }
+    }
+}
