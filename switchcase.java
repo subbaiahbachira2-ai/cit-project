8ad99a7e0a4d21fb8960cedee9eee02ae4c64652
subbaiahbachira2-ai <subbaiahbachira2@gmail.com>
2026-03-26 12:43:42 +0530

    updated controle structure

---
+++ switchcase.java
@@ -0,0 +1,14 @@
+public class switchcase {
+    public static void main(String[] args) {
+        int day=2;
+        switch(day){
+            case 1:
+                System.out.println("monday");
+            case 2:
+                System.out.println("tuesday"); 
+                break;
+            default:
+                System.out.println("invalid day given");       
+        }
+    }
+}
